b76e2601eee9c03e5a8d5e69f9b753d035fb7aae
ztshort <ztshort@hotmail.com>
2022-10-31 15:12:25 -0500

    Added digital oceanscripts and updated contract  addresses

--- src/mappings/pricing.ts
+++ src/mappings/pricing.ts
@@ -3,25 +3,14 @@ import { Pair, Token, Bundle } from '../types/schema'
 import { BigDecimal, Address, BigInt } from '@graphprotocol/graph-ts/index'
 import { ZERO_BD, factoryContract, ADDRESS_ZERO, ONE_BD, UNTRACKED_PAIRS } from './helpers'
 
-const WETH_ADDRESS = '0xcEc1609Efd3f12d0Da63250eF6761A7482Dda3BF'
-const USDT_WETH_PAIR = '0x1fA11A81a09f89Df7aE2A8f0E846B3A3a2706Bc9' // created block 10093341
-
-// token0:  0xCcdC0653935A251B6839F30359917977f994b5d9   USDT
-// token1:  0xcEc1609Efd3f12d0Da63250eF6761A7482Dda3BF   WXDC
+const WETH_ADDRESS = '0xcec1609efd3f12d0da63250ef6761a7482dda3bf'
+const USDT_WETH_PAIR = '0x1fa11a81a09f89df7ae2a8f0e846b3a3a2706bc9' // created block 10093341
 
 export function getEthPriceInUSD(): BigDecimal {
-  // fetch eth prices for each stablecoin
-
-  //let daiPair = Pair.load(DAI_WETH_PAIR) // dai is token0
-  //let usdcPair = Pair.load(USDC_WETH_PAIR) // usdc is token0
-  let usdtPair = Pair.load(USDT_WETH_PAIR) // usdt is token1
+  let usdtPair = Pair.load(USDT_WETH_PAIR) // usdt is token0
 
   if (usdtPair !== null) {
-    let totalLiquidityETH = usdtPair.reserve1
-
-    let usdtWeight = usdtPair.reserve0.div(totalLiquidityETH)
-
-    return usdtPair.token0Price.times(usdtWeight)
+    return usdtPair.token0Price
   } else {
     return ZERO_BD
   }
@@ -29,26 +18,10 @@ export function getEthPriceInUSD(): BigDecimal {
 
 // token where amounts should contribute to tracked volume and liquidity
 let WHITELIST: string[] = [
-  '0xcEc1609Efd3f12d0Da63250eF6761A7482Dda3BF', // WETH
-  // '0x6b175474e89094c44da98b954eedeac495271d0f', // DAI
-  // '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // FTHM
-  '0xCcdC0653935A251B6839F30359917977f994b5d9' // USDT
-  // '0x0000000000085d4780b73119b644ae5ecd22b376', // TUSD
-  // '0x5d3a536e4d6dbd6114cc1ead35777bab948e3643', // cDAI
-  // '0x39aa39c021dfbae8fac545936693ac917d5e7563', // cUSDC
-  // '0x86fadb80d8d2cff3c3680819e4da99c10232ba0f', // EBASE
-  // '0x57ab1ec28d129707052df4df418d58a2d46d5f51', // sUSD
-  // '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2', // MKR
-  // '0xc00e94cb662c3520282e6f5717214004a7f26888', // COMP
-  // '0x514910771af9ca656af840dff83e8264ecf986ca', //LINK
-  // '0x960b236a07cf122663c4303350609a66a7b288c0', //ANT
-  // '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f', //SNX
-  // '0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e', //YFI
-  // '0xdf5e0e81dff6faf3a7e52ba697820c5e32d806a8', // yCurv
-  // '0x853d955acef822db058eb8505911ed77f175b99e', // FRAX
-  // '0xa47c8bf37f92abed4a126bda807a7b7498661acd', // WUST
-  // '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984', // UNI
-  // '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599' // WBTC
+  '0xcec1609efd3f12d0da63250ef6761a7482dda3bf', // WETH
+  '0x4c52500DdC18EE0C6CB6155961347076E43ABb99', // FTHM 
+  '0x32333d7d5aE3Ea3bee41618838842EdA5581576c', // FXD 
+  '0xccdc0653935a251b6839f30359917977f994b5d9', // USDT 
 ]
 
 // minimum liquidity required to count towards tracked volume for pairs with small # of Lps
@@ -108,7 +81,7 @@ export function getTrackedVolumeUSD(
   }
 
   // if less than 5 LPs, require high minimum reserve amount amount or return 0
-  if (pair.liquidityProviderCount.lt(BigInt.fromI32(5))) {
+  if (pair.liquidityProviderCount.lt(BigInt.fromI32(0))) {
     let reserve0USD = pair.reserve0.times(price0)
     let reserve1USD = pair.reserve1.times(price1)
     if (WHITELIST.includes(token0.id) && WHITELIST.includes(token1.id)) {
